2b638922b1a5acaca1e7a3f2428391352d6973d4
Hamza Shahbaz <121577494+HamzaShahbaz0512@users.noreply.github.com>
2024-06-13 16:14:06 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Profile from './Components/Profile/Profile'
+import Introduction from './Components/Introduction/Introduction'
+import Experince from './Components/Experince/Experince'
+import Education from './Components/Education/Education'
+import Hobby from './Components/Hobby/Hobby'
+
+const App = () => {
+  return (
+    <div>
+<Profile/>
+<Introduction/>
+<Experince/>
+<Education/>
+<Hobby/>
+    </div>
+  )
+}
+
+export default App
